e6290f48e845a76ddf36edcf140d0f35a1482be5
Omkar Gawde <dev@ogawde.tech>
2025-06-07 16:35:29 +0530

    chore: initialize Vite + React project

---
+++ app/src/App.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        
+      </Routes>
+    </BrowserRouter>
+  );  
+
+}
+
+export default App;
